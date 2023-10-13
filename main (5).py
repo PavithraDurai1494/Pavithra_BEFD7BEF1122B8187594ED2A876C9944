ec76d77dce7c0d966441418eaa34c8ae82f705a8
PavithraDurai1494 <144252598+PavithraDurai1494@users.noreply.github.com>
2023-10-13 11:21:47 +0530

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,15 @@
+def linearsearchproduct(Productlist,targetproduct):
+  indices=[]
+  for i,product in enumerate(Productlist):
+    if product==targetproduct:
+      indices.append(i)
+  return indices
+products=["apple","banana","apple","orange","apple"]
+target="apple"
+result=linearsearchproduct(products,target)
+if result:
+  print(f"The Product'{target}'was found at indices:{result}")
+else:
+  print(f"The product'{target}'was not found in the list.")
+
+      
